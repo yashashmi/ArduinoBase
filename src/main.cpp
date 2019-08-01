49e5b18b54d26cbbdbe26b3f5b072ab9dfb8dd46
Yasub Hashmi <yas.hashmi@gmail.com>
2019-08-01 11:34:19 +0530

    Included tests and updated the "ini" configuration  file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-#include "Arduino.h"
+#ifndef UNIT_TEST
+
+#include <Arduino.h>
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
@@ -6,21 +8,13 @@
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  
+ 
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(1000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(1000);
+  
 }
+
+#endif
